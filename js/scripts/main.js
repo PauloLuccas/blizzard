5312747c68d89f7b4fa3fff29fd45b8d2001f515
paulo lucas <paulolucas.dev@gmail.com>
2022-03-02 21:53:57 -0300

    Adicionando lógica ao js para abrir e fechar modal de login.

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -35,3 +35,17 @@ allFilters.forEach((filter, index) => {
     filter.classList.add('active');
   });
 });
+
+const btnOpenModal = document.querySelector('.js-open-modal');
+const btnCloseModal = document.querySelector('.js-close');
+
+btnOpenModal.addEventListener('click', (event) => {
+  event.preventDefault();
+  let tagHtml = document.documentElement;
+  tagHtml.classList.add('show-modal');
+});
+
+btnCloseModal.addEventListener('click', () => {
+  let tagHtml = document.documentElement;
+  tagHtml.classList.remove('show-modal');
+});
